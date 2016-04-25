a2645434a731c9c3a998167e452d9f7db1c70ca5
James <james.a.forbes@gmail.com>
2016-04-25 18:26:54 +1000

    Simulating user input

--- Main.java
+++ Main.java
@@ -2,14 +2,24 @@ import java.util.LinkedList;
 
 public class Main {
    public static void main(String[] args){
-       LinkedList<NumberFormatException> errors = new LinkedList<>();
 
-       errors.add(new JamesException("JamesException"));
-       errors.add(new NumberFormatException("NumberFormatException"));
+       System.out.println("\n----");
 
-       for(NumberFormatException err : errors){
+       LinkedList<String> inputs = new LinkedList<>();
+
+       inputs.add("1");
+       inputs.add("10");
+       inputs.add("");
+       inputs.add("-1");
+
+       for(String input : inputs){
            try {
-               throw err;
+               if(input != ""){
+                if( Integer.parseInt(input) < 0){
+                    throw new JamesException("Negative input is not permitted");
+                }
+                System.out.printf("Your input %s is valid\n", input);
+               }
            } catch (NumberFormatException e){
                System.err.println("Caught Exception "+e.toString());
            }
