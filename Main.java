9df6a806b15eac5ae8ad9d9be7e852f37a22b68c
James <james.a.forbes@gmail.com>
2016-04-25 18:03:08 +1000

    Catching caught exception by referencing parent class

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
    public static void main(String[] args){
-       throw new JamesException("What an amazing Exception");
+       try {
+        throw new JamesException("What an amazing Exception");
+       } catch (NumberFormatException e){
+           System.err.println("Caught custom exception");
+       }
    }
 }
